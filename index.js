eb18b31521b94b44489cb96ec4fddbd649253290
Paul Pan <panjunhong05@gmail.com>
2020-03-22 17:44:33 -0700

    Final changes

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const express = require('express');
+const AWS = require("aws-sdk");
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/:route', (req, res) => {
+  const params = {
+    TableName: "customUrlTable",
+    Key:{
+        "route": req.params["route"]
+    }
+  };
+  docClient.get(params, (err, data) => {
+    if (err) {
+      res.status(err.statusCode || 500).send(err.message);
+    } else {
+      res.json(data);
+    }
+  });
 });
+
+app.get('/', (req, res) => {
+  const params = {
+    TableName: "customUrlTable",
+    Select: "ALL_ATTRIBUTES"
+  };
+  docClient.scan(params, (err, data) => {
+    if (err) {
+      res.status(err.statusCode || 500).send(err.message);
+    } else {
+      res.json(data.Items);
+    }
+  });
+})
 
 const server = awsServerlessExpress.createServer(app);
 
